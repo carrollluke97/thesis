e0489c230dae60c2de5e521b06231be095436de6
carrollluke97 <carrollluke86@gmail.com>
2022-11-23 14:14:53 +0000

    MA5114 Assignment 6 code and files.

---
+++ fastqc_files.py
@@ -0,0 +1,25 @@
+import os 
+
+seq_dir="/home/lukec/Documents/MA5114/FastQC/STEAK-master/testdata/test_dataset2"
+fastqc_dir="/home/lukec/Documents/MA5114/FastQC/STEAK-master/fastqc_output"
+
+file_list=os.listdir(seq_dir)
+print(seq_dir)
+
+for seq in file_list:
+    command="fastqc "+seq_dir+"/"+seq
+    print(command)
+    os.system(command)
+
+    command1="mv "+seq_dir+"/"+"*.html "+fastqc_dir
+    print(command1)
+    os.system(command1)
+
+    #command2="mv "+seq_dir+"/"+"*.zip"+fastqc_dir
+    #print(command2)
+    #os.system(command2)
+
+    print("#done")
+
+    # look in settings to see if the environment recognises fastqc .bashrc
+    
